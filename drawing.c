04b305921381ce00a733f059c5939e1fa4c3f597
Guo Yun <46256003+SadKetchup@users.noreply.github.com>
2019-01-30 15:21:06 +0800

    Create drawing.c

---
+++ drawing.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{	
+  srand(time(0));
+	while(1){
+	
+	printf("\t\t|\t        *       \t|\t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t|\t       ***      \t|\t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t|\t      *****     \t|\t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t|\t     *******    \t|\t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t|\t    *********   \t|\t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t|\t   ***********  \t|\t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t|\t  ************* \t|\t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t|\t ***************\t|\t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+					//the middle
+	printf("\t\t \t  ************* \t \t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t \t   ***********  \t \t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t \t    *********   \t \t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t \t     *******    \t \t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t \t      *****     \t \t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+	printf("\t\t \t       ***      \t \t\t%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n",rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10,rand()%10);
+		Sleep(50);
+
+	}
+	return 0 ;
+}
