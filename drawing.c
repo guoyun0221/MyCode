843d7f9e36d1a9048cf53b72e1032d784d05e07a
Guo Yun <46256003+SadKetchup@users.noreply.github.com>
2019-01-30 14:56:30 +0800

    Add files via upload

---
+++ drawing.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+	while(1){
+	
+	printf("\t\t|\t        *       \t|\n");
+		Sleep(50);
+	printf("\t\t|\t       ***      \t|\n");
+		Sleep(50);
+	printf("\t\t|\t      *****     \t|\n");
+		Sleep(50);
+	printf("\t\t|\t     *******    \t|\n");
+		Sleep(50);
+	printf("\t\t|\t    *********   \t|\n");
+		Sleep(50);
+	printf("\t\t|\t   ***********  \t|\n");
+		Sleep(50);
+	printf("\t\t|\t  ************* \t|\n");
+		Sleep(50);
+	printf("\t\t|\t ***************\t|\n");//the middle
+		Sleep(50);
+	printf("\t\t\t  *************\n");
+		Sleep(50);
+	printf("\t\t\t   ***********\n");
+		Sleep(50);
+	printf("\t\t\t    *********\n");
+		Sleep(50);
+	printf("\t\t\t     *******\n");
+		Sleep(50);
+	printf("\t\t\t      *****\n");
+		Sleep(50);
+	printf("\t\t\t       ***\n");
+		Sleep(50);
+	printf("\t\t\t        *\n");
+		Sleep(50);
+				
+	}
+	return 0 ;
+}
