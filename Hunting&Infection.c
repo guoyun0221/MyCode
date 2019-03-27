6a06b89ace6a17417947b5e474cfe288ad8ab85a
Guo Yun <46256003+SadKetchup@users.noreply.github.com>
2019-03-27 18:32:24 +0800

    Update Hunting&Infection.c

--- Hunting&Infection.c
+++ Hunting&Infection.c
@@ -6,12 +6,10 @@
 #include <conio.h>
 
 #define xsize 110
-#define ysize 25
+#define ysize 26
 #define MaxNameLength 9   //演化模式 
 #define unavailable 391   //拯救模式 
-#define hunternamelength 6  //拯救模式 
-#define preynamelength 4  //拯救模式 
-#define playernamelength 8   //拯救模式
+#define playernamelength 8  //拯救模式 + 逃亡模式 
 
 typedef struct{
 	int isplayer;//用来标记身份 
@@ -23,10 +21,13 @@ typedef struct{
 	int y ;
 }object;  //角色的参数. x,y为其坐标	
 
+void Flee(void);
 void Rescue_Prey(void);
 void Watch_Evolution(void);
 int Name_Length(char a[]);
+void initialize(object objects[],int hunternumber,int preynumber,int player,char huntername[],char preyname[]);
 void Repaint(const object a[],const int num);
+void Chase(object a[],int num);
 void Random_Move(int *x ,int *y);
 void Get_player_Move(int *x ,int *y);
 void Update_Location(object a[],int num);
@@ -42,28 +43,80 @@ int main()
 	srand((time(0)));
 	int mode;
 					
-	printf("\n\t\t\t\tHunting And Infection\n\n");
-	printf("猎物碰到猎人会被同化，你可以拯救猎物。小心别被猎人杀死.\n");	//首界面，
-	printf("The prey will be infected when encountering hunters, you could save them. Watch out the hunters.\n");
+	printf("\n\t\t\t\t\tHunting And Infection\n\n");
+	printf("猎物碰到猎人会被同化。你可以拯救猎物。小心别被猎人杀死.\n");	//首界面，
+	printf("The prey will be infected when encountering hunters, you could save them. Watch out the hunters.\n\n");
+	printf("---------------------------\n");
 	
-	printf("\n选择模式:\n");			//选择模式 
-	printf("Choose a way:\n");
-	printf("1: 拯救猎物      \t2: 自然演化\n");
-	printf("1: Rescueing the prey\t2: Watching the evolution\n");
+	printf("选择模式:\n");			//选择模式 
+	printf("Choose a way:\n\n");
+	printf("1: 逃亡   \t2: 拯救猎物          \t3: 自然演化\n");
+	printf("1: fleeing\t2: Rescuing the prey\t3: Watching the evolution\n");
 	scanf("%d",&mode);
 
 	if(mode==1){
-		Rescue_Prey();
+		Flee();
 	}else if(mode==2){
+		Rescue_Prey();
+	}else if(mode==3){			
 		Watch_Evolution();
-	}else{			//处理意外输入 
-		printf("\n滚\nFuck off\n"); 
+	}else{		//处理异常输入 
+		printf("滚!\nFuck off!\n");
 	}
 	
 	sleep(1);			//游戏结束 
 	printf("\nPress enter key to end\n");
 	system("pause");
 	return 0 ;
+}
+
+void Flee(void)
+{
+	int hunternumber;
+	int i;
+	int death = 0;
+	int score = 0;
+	
+	system("cls");			//游戏说明 
+	printf("\n\n按'WASD'移动，别被猎人抓住。\n");
+	printf("Press 'WASD' to move. Don't be caught.\n\n---------------------\n\n");
+	
+	printf("请输入猎人数量: \n");	//创建
+	printf("Set the number of hunters\n");
+	scanf("%d",&hunternumber);
+	int num = hunternumber + 1; //玩家数据用一个元素存储 
+	object hunter_player[num];
+	
+	initialize(hunter_player,hunternumber,0,1,"hunter","");//初始化 
+
+	system("cls");
+	system("color 84");//光标和背景一个颜色，隐藏光标 
+	
+	Reorder(hunter_player,num);	//避免第一次乱画 
+	while(death!=1){		//不死就循环 
+		printf("\n\t\t-----------------------Score: %d----------------------------\n",score);//最上方显示 
+		Repaint(hunter_player,num);	 
+		Chase(hunter_player,num); 		//怪物动作 
+		for(i=0;i<num;++i){	// 	找到玩家元素 
+			if(hunter_player[i].isplayer==1){
+			 	Get_player_Move(&hunter_player[i].x,&hunter_player[i].y);//获取玩家操作 
+		 		break;//减少运算
+			}
+		}
+		death=Encounter(hunter_player,num,0);//没有拯救，给个0  
+		++score;//每轮循环，即玩家每动一下，加一分 
+		Reorder(hunter_player,num);//按坐标对数组元素排序 
+		Sleep(50);
+		system("cls");//清屏，准备下一次重画 
+	}
+			//死了跳出循环 
+	printf("\n\t\t-----------------------Score: %d----------------------------\n",score);
+	Repaint(hunter_player,num);//看看怎么死的
+	sleep(1);
+	system("cls");
+	printf("\n------------------------\n你的得分是: %d\n",score);
+	printf("Your scroe is: %d\n",score);
+	
 }
 
 void Rescue_Prey(void)
@@ -77,8 +130,8 @@ void Rescue_Prey(void)
 	printf("\n\n按'WASD'移动。躲避猎人，拯救猎物。\n");	
 	printf("Press 'WASD' to move. Stay away from hunters and save prey.\n\n---------------\n\n");
 	
-	printf("请输入猎人数量: \n");//参数设置 
-	printf("Set the number hunters: \n");
+	printf("请输入猎人数量: \n");		//创建 
+	printf("Set the number of hunters: \n");
 	scanf("%d",&hunternumber);
 	printf("\n请输入猎物数量:\n");
 	printf("Set the number of prey:\n");
@@ -86,61 +139,39 @@ void Rescue_Prey(void)
 	int number=hunternumber+preynumber+1; 
 	object objects[number];
 	
-	for(i=0;i<hunternumber;++i){//初始化hunter
-		objects[i].ishunter=1;
-		objects[i].isplayer=0;
-		objects[i].isprey=0;
-		strcpy(objects[i].name,"hunter");//字符串是数组不能直接赋值 
-		objects[i].namelength=hunternamelength;		//就不再去调用namelength的函数了，减少运算 
-		objects[i].x=rand()%xsize;//随机生成位置坐标 
-		objects[i].y=rand()%ysize;
-	}
-	for(i;i<number-1;++i){//初始化prey 
-		objects[i].ishunter=0;
-		objects[i].isplayer=0;
-		objects[i].isprey=1;
-		strcpy(objects[i].name,"prey");
-		objects[i].namelength=preynamelength;
-		objects[i].x=rand()%xsize;
-		objects[i].y=rand()%ysize;
-	}
-		objects[i].ishunter=0;		//初始化玩家数据 
-		objects[i].isplayer=1;
-		objects[i].isprey=0;
-		strcpy(objects[i].name,"*player*");
-		objects[i].namelength=playernamelength;
-		objects[i].x=rand()%xsize;
-		objects[i].y=rand()%ysize;
-		
+	initialize(objects,hunternumber,preynumber,1,"hunter","prey");//初始化 
+
 	system("cls");
-	system("color 84");//光标和背景一个颜色，隐藏光标 
+	system("color 84");//隐藏光标 
 	
 	Reorder(objects,number);	//避免第一次乱画 
 	while(1){	//无限根据位置清屏重画 
-		printf("\t\t-------------------Saved prey: %d-----------------------\n",savedprey);//最上方显示 
+		printf("\n\t\t----------------------Saved prey: %d--------------------------\n",savedprey);//最上方显示 
 		Repaint(objects,number);
 		Update_Location(objects,number);	//怪物的随机运动，玩家的运动 ,排序 
-		Sleep(50);
 		if(Encounter(objects,number,&savedprey)){	//encounter返回death 
 			system("cls");
+			printf("\n\t\t----------------------Saved prey: %d--------------------------\n",savedprey);
 			Repaint(objects,number);//看看怎么死的 
 			sleep(1);
 			system("cls");
-			printf("\n---------\n你救下了%d只猎物,最开始有%d只。你死了。\n",savedprey,preynumber);	
+			printf("\n\n--------------\n你救下了%d只猎物,最开始有%d只。你死了。\n",savedprey,preynumber);	
 			printf("You saved %d prey out of %d, and you died.\n",savedprey,preynumber);
 			break;
 		}	
 		if(If_None_prey(objects,number)){  	//没有prey了，游戏结束 
 			system("cls");
 			Reorder(objects,number);//避免闪现 
+			printf("\n\t\t----------------------Saved prey: %d--------------------------\n",savedprey);
 			Repaint(objects,number);//看一下最后的情况 
 			sleep(1);	
 			system("cls");
-			printf("\n----------\n你救下了%d只猎物,最开始有%d只。\n",savedprey,preynumber);	
+			printf("\n\n---------------\n你救下了%d只猎物,最开始有%d只。\n",savedprey,preynumber);	
 			printf("You saved %d prey out of %d.\n",savedprey,preynumber);
 			break;
 		}
 		Reorder(objects,number);//解决玩家a[i]吃掉preya[i-1]之后瞬间跑到a[i-2]后边的问题。 
+		Sleep(50);
 		system("cls");
 	}
 }
@@ -170,24 +201,7 @@ void Watch_Evolution(void)
 	int num=hunternum+preynum;  //数组大小为两种之和 
 	object hunter_prey[num];   //生成猎人和猎物的数组 
 	
-	for(i=0;i<hunternum;++i){   //初始化hunter
-		hunter_prey[i].isplayer=0;
-		hunter_prey[i].ishunter=1;
-		hunter_prey[i].isprey=0;
-		strcpy(hunter_prey[i].name,huntername);  //字符串是数组不能直接赋值 
-		hunter_prey[i].namelength=Name_Length(hunter_prey[i].name);
-		hunter_prey[i].x=rand()%xsize;//随机生成位置坐标 
-		hunter_prey[i].y=rand()%ysize;
-	}
-	for(i;i<num;++i){     //初始化prey 
-		hunter_prey[i].isplayer=0;
-		hunter_prey[i].isprey=1;
-		hunter_prey[i].ishunter=0;
-		strcpy(hunter_prey[i].name,preyname);
-		hunter_prey[i].namelength=Name_Length(hunter_prey[i].name);
-		hunter_prey[i].x=rand()%xsize;
-		hunter_prey[i].y=rand()%ysize;
-	}
+	initialize(hunter_prey,hunternum,preynum,0,huntername,preyname);//初始化各种类参数 
 	
 	system("cls");
 	system("color 84");//隐藏光标 
@@ -211,6 +225,40 @@ int Name_Length(char a[])
 	return count;
 }
 
+void initialize(object objects[],int hunternumber,int preynumber,int player,char huntername[],char preyname[])
+{
+	int num=hunternumber+preynumber+player;
+	int i;
+	
+	for(i=0;i<hunternumber;++i){//初始化hunter
+		objects[i].ishunter=1;
+		objects[i].isplayer=0;
+		objects[i].isprey=0;
+		strcpy(objects[i].name,huntername);
+		objects[i].namelength=Name_Length(huntername);;		
+		objects[i].x=rand()%xsize;
+		objects[i].y=rand()%ysize;
+	}
+	for(i=0;i<preynumber;++i){//初始化prey 
+		objects[hunternumber+i].ishunter=0;
+		objects[hunternumber+i].isplayer=0;
+		objects[hunternumber+i].isprey=1;
+		strcpy(objects[hunternumber+i].name,preyname);
+		objects[hunternumber+i].namelength=Name_Length(preyname);;
+		objects[hunternumber+i].x=rand()%xsize;
+		objects[hunternumber+i].y=rand()%ysize;
+	}
+	if(player){		//有玩家参与的话 
+		objects[num-1].ishunter=0;		//初始化玩家数据 
+		objects[num-1].isplayer=1;		//用最后一个元素存储玩家数据 
+		objects[num-1].isprey=0;
+		strcpy(objects[num-1].name,"*player*");
+		objects[num-1].namelength=playernamelength;
+		objects[num-1].x=rand()%xsize;
+		objects[num-1].y=rand()%ysize;
+	}
+}
+
 void Repaint(const object a[],const int num)
 {
 	int t = 0;
@@ -226,7 +274,7 @@ void Repaint(const object a[],const int num)
 		printf("%s",a[0].name);
 	}
 	for(t=1;t<num;++t){//根据坐标打印剩下的 //因为要判断i-1所以遍历从1开始 
-		if(a[t].x!=unavailable){
+		if(a[t].x!=unavailable){	//去掉被移除屏幕的 
 			if(a[t-1].y<a[t].y){
 				for(;i<a[t].y;++i){
 					printf("\n");
@@ -244,6 +292,29 @@ void Repaint(const object a[],const int num)
 			}
 		}
 	}	
+}
+
+void Chase(object a[],int num)
+{
+	int x,y;	
+	int i ;
+	
+	for(i=0;i<num;++i){		//记录玩家位置
+		if(a[i].isplayer==1){
+			x=a[i].x;
+			y=a[i].y;
+		}
+	}
+	
+	for(i=0;i<num;++i){
+		if(rand()%2){	//随机从x或y方向趋近玩家 
+			if(a[i].x<x) a[i].x+=2;//没有设置等号，如果是玩家自己，坐标不变 
+			if(a[i].x>x) a[i].x-=2;
+		}else{
+			if(a[i].y<y) ++a[i].y;
+			if(a[i].y>y) --a[i].y;
+		} 
+	}
 }
 
 void Random_Move(int *x ,int *y)
@@ -267,10 +338,10 @@ void Get_player_Move(int *x ,int *y)
 {							 
 	char a = getch();//wasd控制坐标变化 从而控制方向 
 	
-	if(a=='w') --(*y);
-	if(a=='s') ++(*y);
-	if(a=='a') (*x)-=2; //横向密集小幅度运动不明显 
-	if(a=='d') (*x)+=2;
+	if(a=='w'||a=='W') --(*y);
+	if(a=='s'||a=='S') ++(*y);
+	if(a=='a'||a=='A') (*x)-=2; //横向密集小幅度运动不明显 
+	if(a=='d'||a=='D') (*x)+=2;
 	
 	if((*x)<0)(*x)=0;  //防止越界 
 	if((*y)<0)(*y)=0;
@@ -324,11 +395,11 @@ int Encounter(object a[],int num,int *savecount)
 {
 	int i;int death =0;
 	for(i=0;i<num-1;++i){
-		if(a[i].y-a[i+1].y>-2){  //纵坐标满足条件 
+		if(a[i].y-a[i+1].y>-2){  //纵坐标满足条件 ,上下相差一行以内 
 			if((a[i].x-a[i+1].x>=-a[i].namelength)&&(a[i].x-a[i+1].x<=a[i+1].namelength)){ //横坐标也满足条件(相距够近) 
 				if(strcmp(a[i].name,a[i+1].name)){   //如果不是同类//strcmp，相同会返回0 
 					if((a[i].ishunter==1&&a[i+1].isplayer==1)||(a[i+1].ishunter==1&&a[i].isplayer==1)){ 
-						death = 1;
+						death = 1; //玩家遇到hunter就死 
 					}
 					else if((a[i].isplayer==1&&a[i+1].isprey==1)||(a[i+1].isplayer==1&&a[i].isprey==1)){
 						if(a[i].isprey==1){
@@ -374,9 +445,8 @@ int If_None_prey(object a[],int num){
 	int i;
 	for(i=0;i<num;++i){
 		if(a[i].isprey==1&&a[i].x!=unavailable){
-			return 0;
+			return 0; 		//只要遇到prey就直接返回0; 
 		}
 	}
-	return 1;
+	return 1;//没有prey返回1; 
 }
-
