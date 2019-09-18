d86faa588024360ac7f1d556bdbcf6c62d1cdb45
Guo Yun <46256003+SadKetchup@users.noreply.github.com>
2019-09-18 22:40:53 +0800

    Update main.go

--- go_spider/main.go
+++ go_spider/main.go
@@ -7,9 +7,11 @@ import (
 
 func main() {
 
-	fmt.Println("Something you can do here (BTW, The code doesn't work for many webpages...)")
-	fmt.Println("A : Get some emails from a page")
-	fmt.Println("B : Get some pictures from a page")
+	fmt.Println("Something you can do here")
+	fmt.Println(" ")
+	fmt.Println("A: Get some emails from a page (BTW, The code doesn't work for many webpages...)")
+	fmt.Println("B: Get some pictures from a page (The code doesn't work for many webpages, either...)")
+	fmt.Println("C: Get 1000 random users' information of Bilibili, including id, name, sex, level and picture")
 
 	var s string
 	fmt.Scanln(&s)
@@ -18,6 +20,8 @@ func main() {
 		spiders.Email()
 	} else if s == "B" || s == "b" {
 		spiders.Img()
+	} else if s == "C" || s == "c" {
+		spiders.Bilibili_user()
 	}
 
 }
