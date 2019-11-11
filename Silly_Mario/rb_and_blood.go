976809dc563b06b21ae414694d9795ad795646c2
Guo Yun <46256003+SadKetchup@users.noreply.github.com>
2019-11-11 19:31:51 +0800

    Update rb_and_blood.go

--- Silly_Mario/rb_and_blood.go
+++ Silly_Mario/rb_and_blood.go
@@ -76,11 +76,11 @@ func gain_blood() {
 	}
 }
 
-func Crash() {
-	for i, p := range rb.point {
+func crash() {
+	for i := 0; i < len(rb.point); i++ {
 		//if player overlaps roadblock//safe width is for the clear area
-		if player.x+safe_width < p.x+roadblock_width && player.x+player_width-safe_width > p.x {
-			if player.y < p.y+roadblock_height && player.y+player_height > p.y {
+		if player.x+safe_width < rb.point[i].x+roadblock_width && player.x+player_width-safe_width > rb.point[i].x {
+			if player.y < rb.point[i].y+roadblock_height && player.y+player_height > rb.point[i].y {
 				player.blood--
 				if player.blood > 0 {
 					rb.point = append(rb.point[:i], rb.point[i+1:]...)
