4085ce3cd00dc938c229a9612819d713930c320c
Guo Yun <46256003+SadKetchup@users.noreply.github.com>
2019-11-11 19:19:05 +0800

    Update crash.go

--- Silly_Mario/crash.go
+++ Silly_Mario/crash.go
@@ -1,10 +1,10 @@
 package main
 
 func crash() {
-	for _, p := range rb.point {
+	for i := 0; i < len(rb.point); i++ {
 		//if player overlaps roadblock//safe width is for the clear area
-		if player.x+safe_width < p.x+roadblock_width && player.x+player_width-safe_width > p.x {
-			if player.y < p.y+roadblock_height && player.y+player_height > p.y {
+		if player.x+safe_width < rb.point[i].x+roadblock_width && player.x+player_width-safe_width > rb.point[i].x {
+			if player.y < rb.point[i].y+roadblock_height && player.y+player_height > rb.point[i].y {
 				player.blood--
 			}
 		}
