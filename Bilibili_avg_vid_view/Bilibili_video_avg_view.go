2197ffdc43b3a47c8b896669e41cc64e2567d38a
Guo Yun <46256003+SadKetchup@users.noreply.github.com>
2020-03-10 20:21:34 +0800

    Update Bilibili_video_avg_view.go

--- Bilibili_avg_vid_view/Bilibili_video_avg_view.go
+++ Bilibili_avg_vid_view/Bilibili_video_avg_view.go
@@ -66,48 +66,35 @@ func insert(vids []Video, vid Video) []Video {
 
 func calculate(vids []Video) {
 	var sum int
-	var gradient [11]int
-	//gradient[0]: view<50;gradient[1]: view>=50 && view<100;100-500;500-1k;1k-5k;5k-10k;10k-50k;
-	//50k-100k;100k-500k;500k-1m;view>1m;
+	var gradient [7]int
+	//gradient[0]: view<10;gradient[1]: view>=10 && view<100;100-1k;1k-10k;10-100k;100k-1m;view>1m;
 	for _, vid := range vids {
 		sum += vid.view
-		if vid.view < 50 {
+		if vid.view < 10 {
 			gradient[0]++
 		} else if vid.view < 100 {
 			gradient[1]++
-		} else if vid.view < 500 {
-			gradient[2]++
 		} else if vid.view < 1000 {
-			gradient[3]++
-		} else if vid.view < 5000 {
-			gradient[4]++
+			gradient[2]++
 		} else if vid.view < 10000 {
-			gradient[5]++
-		} else if vid.view < 50000 {
-			gradient[6]++
+			gradient[3]++
 		} else if vid.view < 100000 {
-			gradient[7]++
-		} else if vid.view < 500000 {
-			gradient[8]++
+			gradient[4]++
 		} else if vid.view < 1000000 {
-			gradient[9]++
+			gradient[5]++
 		} else {
-			gradient[10]++
+			gradient[6]++
 		}
 	}
 	fmt.Println("Sum:", sum)
 	fmt.Println("Average:", sum/len(vids))
 	fmt.Println("Median", vids[len(vids)/2].view)
 	fmt.Println("---------Gradient table---------")
-	fmt.Println("view<50", gradient[0])
-	fmt.Println("50-100", gradient[1])
-	fmt.Println("100~500", gradient[2])
-	fmt.Println("500~1k", gradient[3])
-	fmt.Println("1k~5k", gradient[4])
-	fmt.Println("5k~10k", gradient[5])
-	fmt.Println("10k~50k", gradient[6])
-	fmt.Println("50k~100k", gradient[7])
-	fmt.Println("100k~500k", gradient[8])
-	fmt.Println("500k~1m", gradient[9])
-	fmt.Println("view>1m", gradient[10])
+	fmt.Println("view<10", gradient[0])
+	fmt.Println("10-100", gradient[1])
+	fmt.Println("100~1k", gradient[2])
+	fmt.Println("1k~10k", gradient[3])
+	fmt.Println("10k~100k", gradient[4])
+	fmt.Println("100k~1m", gradient[5])
+	fmt.Println("view>1m", gradient[6])
 }
